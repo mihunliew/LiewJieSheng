dc2aedb35388805dd9103b3c8cd7f57ad9e7757b
mihunliew <62315775+mihunliew@users.noreply.github.com>
2022-10-29 11:13:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.header("My first Streamlit App")
